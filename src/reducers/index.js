0c32bc5d63af3d75ed4a6315317b74f67f13e2e7
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-13 14:02:08 -0300

    Add Post capabilities and init validation of redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer // The key must be form
 });
 
 export default rootReducer;
