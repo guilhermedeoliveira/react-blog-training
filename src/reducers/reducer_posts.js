da9371b3f02e3ae86d937ddffbd70d81968d0bcb
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-14 15:20:47 -0300

    Create post action creator and treat case in reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,15 +1,19 @@
 import _ from 'lodash';
 
- import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
- export default function(state = {}, action) {
+export default function(state = {}, action) {
 
-   switch (action.type) {
-     case FETCH_POSTS:
-        return _.mapKeys(action.payload.data, 'id');
-       break;
+ switch (action.type) {
+   case FETCH_POST:
+      return { ...state, [action.payload.data.id]: action.payload.data };
+     break;
 
-     default:
-      return state;
-   }
+   case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
+     break;
+
+   default:
+    return state;
  }
+}
